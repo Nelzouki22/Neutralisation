5e4ea4abdc65e38bf2e6a5be4892f06e8147be9a
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-10-03 21:13:42 -0400

    Add project files.

---
+++ Neutralisation/Neutralisation.py
@@ -0,0 +1,17 @@
+﻿def neutralise(s1, s2):
+    result = ""
+    for char1, char2 in zip(s1, s2):
+        if char1 == "+" and char2 == "+":
+            result += "+"
+        elif char1 == "-" and char2 == "-":
+            result += "-"
+        else:
+            result += "0"
+    return result
+
+# أمثلة على الاستخدام
+print(neutralise("+-+", "+--"))        # ➞ "+-0"
+print(neutralise("--++--", "++--++"))   # ➞ "000000"
+print(neutralise("-+-+-+", "-+-+-+"))   # ➞ "-+-+-+"
+print(neutralise("-++-", "-+-+"))       # ➞ "-+00"
+
